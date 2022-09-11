b55e4ac0f949b6f12011d3fed422491bcf5313fc
U-DESKTOP-OEJ98CE\amram <amramos0828@gmail.com>
2022-09-11 15:56:46 -0400

    feat: write solution for median of 2 sorted arrs

--- src/main/scala/com/avnatnezter/leetcode/binarysearch/q004medianoftwosortedarrays/Solution.scala
+++ src/main/scala/com/avnatnezter/leetcode/binarysearch/q004medianoftwosortedarrays/Solution.scala
@@ -2,14 +2,33 @@ package com.avnatnezter.leetcode.binarysearch.q004medianoftwosortedarrays
 
 class Solution {
     def findMedianSortedArrays(nums1: Array[Int], nums2: Array[Int]): Double = {
-        val left = nums1
-        val right = nums2
+        val (a, b) = if (nums2.length > nums1.length) (nums1, nums2) else (nums2, nums1)
+        val total = a.length + b.length
+        val half = total / 2
 
-        val partitionSize = (left.length + right.length) / 2
-        val leftPartition2 = right.length / 2
-        val leftPartition1 = Math.abs(partitionSize - leftPartition2)
+        var leftPointer = 0
+        var rightPointer = a.length - 1
 
-        println(s"partition = $partitionSize, left2 = $leftPartition2, left1 = $leftPartition1")
-        0d
+        def leftIndexA: Int = Math.min(leftPointer + rightPointer, (leftPointer + rightPointer) / 2)
+        def leftIndexB: Int = half - leftIndexA - 2
+
+        def leftValueA: Int = if (leftIndexA >= 0) a(leftIndexA) else Int.MinValue
+        def rightValueA: Int = if (leftIndexA + 1 < a.length) a(leftIndexA + 1) else Int.MaxValue
+        def leftValueB: Int = if (leftIndexB >= 0) b(leftIndexB) else Int.MinValue
+        def rightValueB: Int = if (leftIndexB + 1 < b.length) b(leftIndexB + 1) else Int.MaxValue
+
+        def moveRightPointer: Boolean = leftValueA > rightValueB
+        def moveLeftPointer: Boolean = leftValueB > rightValueA
+
+        while (moveRightPointer || moveLeftPointer)
+            if (moveRightPointer)
+                rightPointer = leftIndexA - 1
+            else
+                leftPointer = leftIndexA + 1
+
+        if (total % 2 == 0)
+            (Math.max(leftValueA, leftValueB) + Math.min(rightValueA, rightValueB)) / 2d
+        else
+            Math.min(rightValueA, rightValueB)
     }
 }
--- src/test/scala/binarysearch/q004medianoftwosortedarrays/SolutionTest.scala
+++ src/test/scala/binarysearch/q004medianoftwosortedarrays/SolutionTest.scala
@@ -5,7 +5,38 @@ import org.scalatest.matchers.should
 import com.avnatnezter.leetcode.binarysearch.q004medianoftwosortedarrays.Solution
 
 class SolutionTest extends AnyFlatSpec with should.Matchers {
-    "test" should "pass" in {
-        new Solution().findMedianSortedArrays(Array(1,2,3,4,5,6,7,9), Array(5,6,7)) should be (2d)
+    "B is the median" should "should return B as the median = 2" in {
+        val A = Array(1,3)
+        val B = Array(2)
+
+        new Solution().findMedianSortedArrays(A, B) should be (2)
+    }
+
+    "A is left partition B is right partition" should "should return avg of left and right (2,3) = 2.5" in {
+        val A = Array(1,2)
+        val B = Array(3,4)
+
+        new Solution().findMedianSortedArrays(A, B) should be (2.5)
+    }
+
+    "A + B is even" should "take average of 2 right partition values (3,4) = 3.5" in {
+        val A = Array(1,2,3,4,5,6,7,8)
+        val B = Array(1,2,3,4)
+
+        new Solution().findMedianSortedArrays(A, B) should be (3.5)
+    }
+
+    "A + B is odd" should "should return min of 2 right partition values (4, 4) = 4" in {
+        val A = Array(1,2,3,4,5,6,7,8)
+        val B = Array(1,2,3,4,5)
+
+        new Solution().findMedianSortedArrays(A, B) should be (4)
+    }
+
+    "A + B is odd" should "should return min of 2 right partition values (4, 5) = 4" in {
+        val A = Array(1,2,3,4,5,6,7,8)
+        val B = Array(1,2,3,5,5)
+
+        new Solution().findMedianSortedArrays(A, B) should be (4)
     }
 }
