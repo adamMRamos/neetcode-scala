93ccb089bf5dee70b0390100ad0a491a57c916fa
U-DESKTOP-OEJ98CE\amram <amramos0828@gmail.com>
2022-09-17 21:03:17 -0400

    feat: finish solution for q015 three sum

--- src/test/scala/twopointers/q015threesum/SolutionTest.scala
+++ src/test/scala/twopointers/q015threesum/SolutionTest.scala
@@ -8,8 +8,8 @@ class SolutionTest extends AnyFlatSpec with should.Matchers {
     "test" should "work" in {
         Solution.threeSum(Array(-1,0,1,2,-1,-4)) should be (
             List(
-                List(-1,-1,2),
-                List(-1,0,1)
+                List(-1,-1, 2),
+                List(-1, 0, 1)
             )
         )
     }
@@ -20,5 +20,23 @@ class SolutionTest extends AnyFlatSpec with should.Matchers {
 
     "test3" should "work" in {
         Solution.threeSum(Array(0,0,0)) should be (List(List(0,0,0)))
+    }
+
+    "test4" should "work" in {
+        Solution.threeSum(Array(1,-1,-1,0)) should be (List(List(-1,0,1)))
+    }
+
+    "test5" should "work" in {
+        Solution.threeSum(Array(-2,0,0,2,2)) should be (List(List(-2,0,2)))
+    }
+
+    "test6" should "work" in {
+        Solution.threeSum(Array(3,0,-2,-1,1,2)) should be (
+            List(
+                List(-2,-1, 3),
+                List(-2, 0, 2),
+                List(-1, 0, 1),
+            )
+        )
     }
 }
