3ea988d6c1f7730c8246725c01380095d6a7d02b
U-DESKTOP-OEJ98CE\amram <amramos0828@gmail.com>
2022-09-17 21:29:07 -0400

    feat: initial code for q017

---
+++ src/test/scala/backtracking/q017lettercombinationsofaphonenumber/SolutionTest.scala
@@ -0,0 +1,21 @@
+package backtracking.q017lettercombinationsofaphonenumber
+
+import com.avnatnezter.leetcode.backtracking.q017lettercombinationsofaphonenumber.Solution
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class SolutionTest extends AnyFlatSpec with should.Matchers {
+    "test1" should "work" in {
+        Solution.letterCombinations("23") should be (
+            List("ad","ae","af","bd","be","bf","cd","ce","cf")
+        )
+    }
+
+    "test1" should "work" in {
+        Solution.letterCombinations("") should be (Nil)
+    }
+
+    "test1" should "work" in {
+        Solution.letterCombinations("2") should be (List("a","b","c"))
+    }
+}
