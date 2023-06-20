b8581b1d0418633ef227ddbbb2da1b2c5110636b
U-DESKTOP-OEJ98CE\amram <amramos0828@gmail.com>
2023-06-19 21:54:18 -0400

    feat: initial solution setup

---
+++ src/main/scala/com/avnatnezter/leetcode/arraysandhashing/q49groupanagrams/Solution.scala
@@ -0,0 +1,7 @@
+package com.avnatnezter.leetcode.arraysandhashing.q49groupanagrams
+
+object Solution {
+    def groupAnagrams(strs: Array[String]): List[List[String]] = {
+        ???
+    }
+}
---
+++ src/test/scala/arraysandhashing/q49groupanagrams/SolutionTest.scala
@@ -0,0 +1,27 @@
+package arraysandhashing.q49groupanagrams
+
+import com.avnatnezter.leetcode.arraysandhashing.q49groupanagrams.Solution
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class SolutionTest extends AnyFlatSpec with should.Matchers {
+    "test1" should "work" in {
+        Solution.groupAnagrams(Array("eat","tea","tan","ate","nat","bat")) shouldEqual List(
+            List("bat"),
+            List("nat","tan"),
+            List("ate","eat","tea")
+        )
+    }
+
+    "test2" should "work" in {
+        Solution.groupAnagrams(Array("")) shouldEqual List(
+            List(""),
+        )
+    }
+
+    "test3" should "work" in {
+        Solution.groupAnagrams(Array("a")) shouldEqual List(
+            List("a"),
+        )
+    }
+}
