3fc370c68a1abf01e2cef5363f90e749428926a0
U-DESKTOP-OEJ98CE\amram <amramos0828@gmail.com>
2023-06-23 16:19:50 -0400

    feat: implement neetcode's solution

--- src/test/scala/arraysandhashing/q128longestconsecutivesequence/SolutionTest.scala
+++ src/test/scala/arraysandhashing/q128longestconsecutivesequence/SolutionTest.scala
@@ -1,10 +1,19 @@
 package arraysandhashing.q128longestconsecutivesequence
 
+import com.avnatnezter.leetcode.arraysandhashing.q128longestconsecutivesequence.Solution
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class SolutionTest extends AnyFlatSpec with should.Matchers {
     "test1" should "work" in {
+        Solution.longestConsecutive(Array()) shouldEqual 0
+    }
 
+    "test2" should "work" in {
+        Solution.longestConsecutive(Array(100,4,200,1,3,2)) shouldEqual 4
+    }
+
+    "test3" should "work" in {
+        Solution.longestConsecutive(Array(0,3,7,2,5,8,4,6,0,1)) shouldEqual 9
     }
 }
