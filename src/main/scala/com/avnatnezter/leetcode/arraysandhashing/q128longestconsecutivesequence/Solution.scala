0391a25330046fa2918513917e49421d7b6be104
U-DESKTOP-OEJ98CE\amram <amramos0828@gmail.com>
2023-06-23 15:27:27 -0400

    feat: initial solution setup

---
+++ src/main/scala/com/avnatnezter/leetcode/arraysandhashing/q128longestconsecutivesequence/Solution.scala
@@ -0,0 +1,7 @@
+package com.avnatnezter.leetcode.arraysandhashing.q128longestconsecutivesequence
+
+object Solution {
+    def longestConsecutive(nums: Array[Int]): Int = {
+        ???
+    }
+}
---
+++ src/test/scala/arraysandhashing/q128longestconsecutivesequence/SolutionTest.scala
@@ -0,0 +1,10 @@
+package arraysandhashing.q128longestconsecutivesequence
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+class SolutionTest extends AnyFlatSpec with should.Matchers {
+    "test1" should "work" in {
+
+    }
+}
