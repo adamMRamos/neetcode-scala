3c556052667784c5e9c6f41c7e9120ad3fcf43b5
U-DESKTOP-OEJ98CE\amram <amramos0828@gmail.com>
2023-06-26 22:18:46 -0400

    feat: implement solution with stack and tests

--- src/main/scala/com/avnatnezter/leetcode/stack/q020validparenthesis/Solution.scala
+++ src/main/scala/com/avnatnezter/leetcode/stack/q020validparenthesis/Solution.scala
@@ -1,7 +1,22 @@
 package com.avnatnezter.leetcode.stack.q020validparenthesis
 
+import scala.collection.mutable
+
 object Solution {
     def isValid(s: String): Boolean = {
-        ???
+        def opposite(c: Char): Option[Char] = c match {
+            case '(' => Some(')')
+            case '{' => Some('}')
+            case '[' => Some(']')
+            case _ => None
+        }
+
+        val stack = mutable.Stack.empty[Char]
+        for (c <- s)
+            if (stack.headOption.flatMap(opposite).contains(c))
+                stack.pop()
+            else
+                stack.push(c)
+        stack.isEmpty
     }
 }
