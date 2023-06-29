08d9ac41db58d5a050d71ac48087a0f6b9aa19a8
U-DESKTOP-OEJ98CE\amram <amramos0828@gmail.com>
2023-06-29 10:44:59 -0400

    feat: implement solution using stack as underlying

--- src/main/scala/com/avnatnezter/leetcode/stack/q155minstack/MinStack.scala
+++ src/main/scala/com/avnatnezter/leetcode/stack/q155minstack/MinStack.scala
@@ -3,22 +3,24 @@ package com.avnatnezter.leetcode.stack.q155minstack
 import scala.collection.mutable
 
 class MinStack() {
-    val stack = mutable.Stack.empty[(Int, Int)]
+    val stack: mutable.Stack[(Int, Int)] = mutable.Stack.empty[(Int, Int)]
 
     def push(value: Int) {
-        stack.push(value -> 0)
+        stack.push(
+            value -> (if (stack.isEmpty) value else Math.min(value, getMin()))
+        )
     }
 
     def pop() {
-
+        stack.pop()
     }
 
     def top(): Int = {
-        ???
+        stack.head._1
     }
 
     def getMin(): Int = {
-        ???
+        stack.head._2
     }
 
 }
--- src/test/scala/stack/q155minstack/MinStackTest.scala
+++ src/test/scala/stack/q155minstack/MinStackTest.scala
@@ -1,10 +1,18 @@
 package stack.q155minstack
 
+import com.avnatnezter.leetcode.stack.q155minstack.MinStack
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class MinStackTest extends AnyFlatSpec with should.Matchers {
     "test1" should "work" in {
-
+        val minStack = new MinStack()
+        minStack.push(-2)
+        minStack.push(0)
+        minStack.push(-3)
+        minStack.getMin() shouldEqual -3
+        minStack.pop()
+        minStack.top() shouldEqual 0
+        minStack.getMin() shouldEqual -2
     }
 }
