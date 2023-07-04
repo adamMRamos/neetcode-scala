d0fc353d1d5932732b45e5ad06a0ce8988056d3d
U-DESKTOP-OEJ98CE\amram <amramos0828@gmail.com>
2023-07-04 17:15:09 -0400

    feat: try to solve with n^2 operation using 2 pointers

--- src/main/scala/com/avnatnezter/leetcode/twopointers/q042trappingrainwater/Solution.scala
+++ src/main/scala/com/avnatnezter/leetcode/twopointers/q042trappingrainwater/Solution.scala
@@ -2,6 +2,28 @@ package com.avnatnezter.leetcode.twopointers.q042trappingrainwater
 
 object Solution {
     def trap(height: Array[Int]): Int = {
-        ???
+        var left = 0
+        var right = 1
+        var volumeOffset = 0
+
+        def area: Int =
+            Math.min(height(left), height(right)) * (right - left) - volumeOffset
+
+        var volume = 0
+        while (left < height.length) {
+            if (height(left)  <= height(right)) {
+                volume = area
+                left = right
+                volumeOffset = 0
+            } else if (right >= height.length) {
+                left += 1
+                right = left + 1
+            } else {
+                volumeOffset += height(right)
+                right += 1
+            }
+        }
+
+        volume
     }
 }
