76f87e69fe1fea8251821e561889e58195dd045f
U-DESKTOP-OEJ98CE\amram <amramos0828@gmail.com>
2023-07-05 10:03:12 -0400

    feat: implement solution using both linear memory and two pointers for constant memory

--- src/main/scala/com/avnatnezter/leetcode/twopointers/q042trappingrainwater/Solution.scala
+++ src/main/scala/com/avnatnezter/leetcode/twopointers/q042trappingrainwater/Solution.scala
@@ -4,6 +4,10 @@ import scala.collection.mutable
 
 object Solution {
     def trap(height: Array[Int]): Int = {
+        trapTwoPointers(height)
+    }
+
+    private def trapLinearMemory(height: Array[Int]): Int = {
         val maxLeft = mutable.ArrayBuffer.fill(height.length)(0)
         val maxRight = mutable.ArrayBuffer.fill(height.length)(0)
 
@@ -17,6 +21,27 @@ object Solution {
         var volume = 0
         for (i <- height.indices)
             volume += Math.max(Math.min(maxLeft(i), maxRight(i)) - height(i), 0)
+
+        volume
+    }
+
+    private def trapTwoPointers(height: Array[Int]): Int = {
+        var volume = 0
+
+        var left = 0
+        var right = height.length - 1
+        var maxLeft = height(left)
+        var maxRight = height(right)
+        while (left < right)
+            if (maxRight < maxLeft) {
+                volume += Math.max(maxRight - height(right), 0)
+                right -= 1
+                maxRight = Math.max(height(right), maxRight)
+            } else {
+                volume += Math.max(maxLeft - height(left), 0)
+                left += 1
+                maxLeft = Math.max(height(left), maxLeft)
+            }
 
         volume
     }
